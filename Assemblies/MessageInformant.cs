427d9b9187fd5ea50ba694a6aa5dbffe1e68b002
Mikhail Novikau <melmenx@gmail.com>
2022-05-19 17:37:30 +0300

    Assembly Message

---
+++ Assemblies/MessageInformant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitHelperInformatMessage
+{
+    public class MessageInformant
+    {
+        /// <summary>
+        /// Displays an error message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static void ErrorOutput(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error: {message}");
+            Console.ResetColor();
+        }
+        /// <summary>
+        /// Displays a success message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static void SuccessOutput(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Success: {message}");
+            Console.ResetColor();
+        }
+    }
+}
